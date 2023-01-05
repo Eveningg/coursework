21db6d6e571811c50a0c86e5dc4558a41a1ac382
Eveningg <93998243+Eveningg@users.noreply.github.com>
2023-01-05 17:12:41 +0000

    added placeholder login page

--- resources/views/back/pages/auth/login.blade.php
+++ resources/views/back/pages/auth/login.blade.php
@@ -1,1 +1,7 @@
-@extends('back')
+@extends('back.layouts.auth-layout')
+@section('pageTitle', isset($pageTitle) ? $pageTitle : 'Login')
+@section('content')
+
+this is a placeholder login password_get_info
+
+@endsection
