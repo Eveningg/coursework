2bbf31e35e5769b5bb7ab960de7099eeb5204665
Eveningg <93998243+Eveningg@users.noreply.github.com>
2023-01-06 13:56:30 +0000

    fixed error

--- app/Http/Livewire/AuthorLoginForm.php
+++ app/Http/Livewire/AuthorLoginForm.php
@@ -18,7 +18,7 @@ class AuthorLoginForm extends Component
             $this -> validate([
                 'login_id'=>'required|email|exists:users,email',
                 'password'=>'required|min:5',
-            ].[
+            ],[
                 'login_id'=>'Enter Your Email Address',
                 'login_id.email'=>'Invalid Email Address.',
                 'login_id.exists'=>'This email is not registered!',
@@ -50,30 +50,6 @@ class AuthorLoginForm extends Component
             session()->flash('fail', 'Incorrect Email/Username or Password!');
         }
 
-        // $this->validate([
-        //     'email'=>'required|email|exists:users,email',
-        //     'password'=>'required|min:5'
-        // ],[ 
-        //     'email.required'=>'Enter Your Email Address',
-        //     'email.email'=>'Invalid Email Address.',
-        //     'email.exists'=>'This email is not registered!',
-        //     'password.required'=>'A Password is Required.'
-        // ]);
-
-        // $creds = array('email'=>$this->email, 'password'=>$this->password);
-
-        // if(Auth::guard('web')->attempt($creds) ){
-            
-        //     $checkUser = User::where('email', $this->email)->first();
-        //     if($checkUser->blocked==1){
-        //         Auth::guard('web')->logout();
-        //         return redirect()->route('author.login')->with('fail', "Your Account has been Blocked.");
-        //     }else{
-        //         return redirect()->route('author.home');
-        //     }
-        // }else{
-        //     session()->flash('fail', 'Incorrect Email or Password!');
-        // }
     }
 
     public function render()
