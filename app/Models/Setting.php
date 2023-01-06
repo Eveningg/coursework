c43a6f656603bb72bc33b89f239d3047f7a8122b
Eveningg <93998243+Eveningg@users.noreply.github.com>
2023-01-06 22:45:18 +0000

    creating thread models/migrations

---
+++ app/Models/Setting.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Setting extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'thread_name',
+        'thread_email',
+        'thread_description',
+        'thread_logo',
+        'thread_favicon',
+    ];
+}
