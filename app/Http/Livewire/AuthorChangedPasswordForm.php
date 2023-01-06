6d31e35cabda315722f711e7a9f25bd561aa0583
Eveningg <93998243+Eveningg@users.noreply.github.com>
2023-01-06 19:39:20 +0000

    completed change password layout

---
+++ app/Http/Livewire/AuthorChangedPasswordForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class AuthorChangedPasswordForm extends Component
+{
+    public function render()
+    {
+        return view('livewire.author-changed-password-form');
+    }
+}
--- resources/views/back/pages/profile.blade.php
+++ resources/views/back/pages/profile.blade.php
@@ -20,16 +20,39 @@
         </div>
         <div class="card-body">
             <div class="tab-content">
-            <div class="tab-pane active show" id="tabs-details">
-                <h4>Home tab</h4>
-                <div>
-                    @livewire('author-personal-details')
+                <div class="tab-pane active show" id="tabs-details">
+                    <h4>Home tab</h4>
+                    <div>
+                        @livewire('author-personal-details')
+                    </div>
                 </div>
-            </div>
-            <div class="tab-pane" id="tabs-password">
-                <h4>Profile tab</h4>
-                <div>Fringilla egestas nunc quis tellus diam rhoncus ultricies tristique enim at diam, sem nunc amet, pellentesque id egestas velit sed</div>
-            </div>
+                <div class="tab-pane" id="tabs-password">
+                    <div>
+                        <form method="post">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="mb-3">
+                                        <label class="form-label">Current Password</label>
+                                        <input type="password" class="form-control" name="example-text-input" placeholder="Current Password:">
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="mb-3">
+                                        <label class="form-label">New Password</label>
+                                        <input type="password" class="form-control" name="example-text-input" placeholder="New Password:">
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="mb-3">
+                                        <label class="form-label">Confirm New Password</label>
+                                        <input type="password" class="form-control" name="example-text-input" placeholder="Confirm New Password:">
+                                    </div>
+                                </div>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Change Password</button>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
