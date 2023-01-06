f6326a2b8078092a49901bc29a20e7abfe878b5e
Eveningg <93998243+Eveningg@users.noreply.github.com>
2023-01-06 01:36:14 +0000

    completed author login form

--- app/Http/Livewire/AuthorLoginForm.php
+++ app/Http/Livewire/AuthorLoginForm.php
@@ -12,7 +12,23 @@ class AuthorLoginForm extends Component
     public $email, $password;
 
     public function LoginHandler(){
-        
+        $this->validate([
+            'email'=>'required|email|exists:user,email',
+            'password'=>'required|min:5'
+        ],[
+            'email.required'=>'Enter Your Email Address',
+            'email.email'=>'invalid Email Address.',
+            'email.exists'=>'This email is not registered!',
+            'password.required'=>'A Password is Required.'
+        ]);
+
+        $creds = array('email' => $this->email, 'password'=>$this->password);
+
+        if(Auth::guard('web')->attempt($creds) ){
+
+        }else{
+            session()->flash('fail', 'Incorrect Email or Password!');
+        }
     }
 
     public function render()
