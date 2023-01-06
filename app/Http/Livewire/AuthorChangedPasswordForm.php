979510bd5664b8fd73b56906d08fcca6e0673bb9
Eveningg <93998243+Eveningg@users.noreply.github.com>
2023-01-06 20:15:58 +0000

    created verification for author

--- app/Http/Livewire/AuthorChangedPasswordForm.php
+++ app/Http/Livewire/AuthorChangedPasswordForm.php
@@ -3,9 +3,30 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthorChangedPasswordForm extends Component
 {
+    public $current_password, $new_password, $confirm_new_password;
+
+    public function changePassword(){
+        $this->validate([
+            'current_password'=>[
+                'required', function($attribute, $value, $fail){
+                    if(!Hash::check($value, User::find(auth('web')->id())->password)){
+                        return $fail(_('The Current Password is Incorrect!'));
+                    }
+                }
+            ],
+            'new_password'=>'required|min:5|max:25',
+            'confirm_new_password'=>'same:new_password'
+        ],[
+            'current_password.required'=>'Enter Your Current Password:',
+            'new_password.required'=>'Enter New Password:',
+            'confirm_new_password.same'=>'The Confirmation Password must Equal New Password!'
+        ]);
+    }
     public function render()
     {
         return view('livewire.author-changed-password-form');
--- resources/views/back/pages/profile.blade.php
+++ resources/views/back/pages/profile.blade.php
@@ -28,29 +28,7 @@
                 </div>
                 <div class="tab-pane" id="tabs-password">
                     <div>
-                        <form method="post">
-                            <div class="row">
-                                <div class="col-md-4">
-                                    <div class="mb-3">
-                                        <label class="form-label">Current Password</label>
-                                        <input type="password" class="form-control" name="example-text-input" placeholder="Current Password:">
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="mb-3">
-                                        <label class="form-label">New Password</label>
-                                        <input type="password" class="form-control" name="example-text-input" placeholder="New Password:">
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="mb-3">
-                                        <label class="form-label">Confirm New Password</label>
-                                        <input type="password" class="form-control" name="example-text-input" placeholder="Confirm New Password:">
-                                    </div>
-                                </div>
-                            </div>
-                            <button type="submit" class="btn btn-primary">Change Password</button>
-                        </form>
+                        @livewire('author-changed-password-form')
                     </div>
                 </div>
             </div>
