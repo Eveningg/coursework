7b4e247931e7d3d3ce0ead52ac0f258d17d3f251
Eveningg <93998243+Eveningg@users.noreply.github.com>
2023-01-06 01:30:09 +0000

    made author login form

--- app/Http/Livewire/AuthorLoginForm.php
+++ app/Http/Livewire/AuthorLoginForm.php
@@ -3,9 +3,18 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AuthorLoginForm extends Component
 {
+
+    public $email, $password;
+
+    public function LoginHandler(){
+        
+    }
+
     public function render()
     {
         return view('livewire.author-login-form');
