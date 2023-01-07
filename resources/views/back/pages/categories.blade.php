2b3acbbe456897f5dd9909a38f6934d4b7ccc5ce
Eveningg <93998243+Eveningg@users.noreply.github.com>
2023-01-07 17:26:33 +0000

    categories event listeners

--- resources/views/back/pages/categories.blade.php
+++ resources/views/back/pages/categories.blade.php
@@ -1,6 +1,39 @@
 @extends('back.layouts.pages-layout')
-@section('pagesTitle', isset($pageTitle) ? $pageTitle : 'Categories')
+@section('pageTitles', isset($pageTitle) ? $pageTitle : 'Categories')
 @section('content')
-    page content here...
-    
+   
+<div class="page-header d-print-none">
+    <div class="row align-items-center">
+      <div class="col">
+        <h2 class="page-title">
+          Categories & Subcategories
+        </h2>
+      </div>
+    </div>
+  </div>
+
+  @livewire('categories')
+
 @endsection
+@push('scripts')
+  <script>
+    window.addEventListener('hideCategoriesModal', function(e){
+      $('#categories_modal').modal('hide');
+    });
+    window.addEventListener('showcategoriesModal', function(e){
+      $('#categories_modal').modal('show');
+    });
+
+    window.addEventListener('hideSubCategoriesModal', function(e){
+      $('#subcategories_modal').modal('hide');
+    });
+
+    window.addEventListener('showSubCategoriesModal', function(e){
+      $('#subcategories_modal').modal('show');
+    });
+
+    $('#categories_modal,#subcategories_modal').on('hidden.bs.modal', function(e){
+      Livewire.emit('resetModalForm');
+    });
+  </script>
+@endpush
