2f9e23c122299ffd5a55905a680b116e218c0cb1
Eveningg <93998243+Eveningg@users.noreply.github.com>
2023-01-07 20:41:45 +0000

    importing sluggable

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,11 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentsSluggable\Sluggable;
 
 class Post extends Model
 {
     use HasFactory;
-
+    use Sluggable;
+    
     protected $fillable = [
         'author_id',
         'category_id',
