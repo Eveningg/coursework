f4a75a42b5be21985fd8847a518f2a4b3af77a43
Eveningg <93998243+Eveningg@users.noreply.github.com>
2023-01-07 17:31:03 +0000

    created models for sub category/category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'category_name',
+        'ordering',
+    ];
+
+    public function subcategories(){
+        return $this->hasMany(SubCategory::class,'parent_category','id');
+    }
 }
--- app/Models/SubCategory.php
+++ app/Models/SubCategory.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class SubCategory extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'subcategory_name',
+        'slug',
+        'parent_category',
+        'ordering',
+    ];
+
+    public function parentcategory(){
+        // return $this->hasOne(Category::class,'id','parent_category');
+        return $this->belongsTo(Category::class,'parent_category','id');
+    }
+
+    public function posts(){
+        return $this->hasMany(Post::class,'category_id','id');
+    }
 }
