d170239e72c330b5a937414f9a770aae63980420
Eveningg <93998243+Eveningg@users.noreply.github.com>
2023-01-07 04:15:26 +0000

    improved layout

--- app/Http/Livewire/Authors.php
+++ app/Http/Livewire/Authors.php
@@ -6,6 +6,20 @@ use Livewire\Component;
 use App\Models\User;
 class Authors extends Component
 {
+    public $name, $email, $username, $author_type, $direct_publisher;
+
+    public function addAuthor(){
+        $this->validate([
+            'name'=>'required',
+            'email'=>'required|email|unique:users,email',
+            'username'=>'required|unique:users,username|min:6|max:25',
+            'author_type'=>'required',
+            'direct_publisher'=>'required',
+        ],[
+            'author_type.required'=>'Choose Author Type',
+            'direct_publisher.required'=>'Specify Author Publication Access',
+        ]);
+    }
     public function render()
     {
         return view('livewire.authors',[
