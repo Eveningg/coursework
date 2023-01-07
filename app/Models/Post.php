93bfa76133b07c2f95e449211bc38a9f0decfa67
Eveningg <93998243+Eveningg@users.noreply.github.com>
2023-01-07 20:07:18 +0000

    migrated posts file

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'author_id',
+        'category_id',
+        'post_title',
+        'post_slug',
+        'post_content',
+        'featured_image',
+    ];
 }
