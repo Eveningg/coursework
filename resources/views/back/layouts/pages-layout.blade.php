7f676da4c5090eac214c6d8c08fd6a63baf6b5a2
Eveningg <93998243+Eveningg@users.noreply.github.com>
2023-01-08 19:03:49 +0000

    bug fixes

--- resources/views/back/layouts/pages-layout.blade.php
+++ resources/views/back/layouts/pages-layout.blade.php
@@ -10,15 +10,15 @@
     <link href="/back/dist/css/tabler-flags.min.css" rel="stylesheet"/>
     <link href="/back/dist/css/tabler-payments.min.css" rel="stylesheet"/>
     <link href="/back/dist/css/tabler-vendors.min.css" rel="stylesheet"/>
-    <link rel="stylesheet" href='/back/dist/libs/ijabo/ijabo.min.css'>
-    <link rel="stylesheet" href='/back/dist/libs/ijaboCropTool/ijaboCropTool.min.css'>
+    <link rel="stylesheet" href="/back/dist/libs/ijabo/ijabo.min.css">
+    <link rel="stylesheet" href="/back/dist/libs/ijaboCropTool/ijaboCropTool.min.css">
     @stack('stylesheets')
-    @livewireScripts
+    @livewireStyles
     <link href="/back/dist/css/demo.min.css" rel="stylesheet"/>
   </head>
   <body >
     <div class="wrapper">
-      @include('back.layouts.inc.header')
+     @include('back.layouts.inc.header')
       <div class="page-wrapper">
         <div class="container-xl">
           <!-- Page title -->
@@ -34,10 +34,10 @@
     </div>
 
     <!-- Libs JS -->
-    <script src="/back/dist/libs/jquery/jquery-3.6.0.min.js'"></script>
-    <script src="/back/dist/libs/ijabo/ijabo.min.js'"></script>
-    <script src="/back/dist/libs/ijaboCropTool/ijaboCropTool.min.js'"></script>
-    <script src="/back/dist/libs/ijaboViewer/jquery.ijaboViewer.min.js'"></script>
+    <script src="/back/dist/libs/jquery/jquery-3.6.0.min.js"></script>
+    <script src="/back/dist/libs/ijabo/ijabo.min.js"></script>
+    <script src="/back/dist/libs/ijaboCropTool/ijaboCropTool.min.js"></script>
+    <script src="/back/dist/libs/ijaboViewer/jquery.ijaboViewer.min.js"></script>
     <script src="/back/dist/libs/apexcharts/dist/apexcharts.min.js"></script>
     <!-- Tabler Core -->
     <script src="/back/dist/js/tabler.min.js"></script>
@@ -45,18 +45,18 @@
     @livewireScripts
     <script>
       window.addEventListener('showToastr', function(event){
-        toastr.remove();
-        if(event.detail.type === 'info'){
-          toastr.info(event.detail.message);
-        }else if(event.detail.type === 'success'){
-          toastr.success(event.detail.message);
-        }else if(event.detail.type === 'error'){
-          toastr.error(event.detail.message);
-        }else if(event.detail.type === 'warning'){
-          toastr.warning(event.detail.message);
-        }else{
-          return false;
-        }
+           toastr.remove();
+           if(event.detail.type === 'info'){
+             toastr.info(event.detail.message);
+           }else if(event.detail.type === 'success'){
+             toastr.success(event.detail.message);
+           }else if(event.detail.type === 'error'){
+             toastr.error(event.detail.message);
+          }else if(event.detail.type === 'warning'){
+            toastr.warning(event.detail.message);
+          }else{
+            return false;
+          }
       });
     </script>
     <script src="/back/dist/js/demo.min.js"></script>
